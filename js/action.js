1b524d94eb35473dd771a557a1e431b8ebddac2f
Muwanguzi Silas <54215685+Silas-seal@users.noreply.github.com>
2022-08-12 08:13:55 +0300

    Do respo in jQuery

--- js/action.js
+++ js/action.js
@@ -2,6 +2,10 @@ console.log('Starting resume application');
 
 jQuery(document).ready(function() {
     jQuery('.loader').fadeOut(1500);
+    jQuery('#quicklinks').css({'max-width' : '1450px'});
+    jQuery('#rsprojects').css({'max-width' : '1450px'});
+    jQuery('#airprojects').css({'max-width' : '1450px'});
+    console.log('Updated on load');
 });
 
 function sayHello() {
