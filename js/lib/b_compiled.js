57499be31ec863ab225bbf84f4afc6bb45a5a4c1
Muwanguzi Silas <54215685+Silas-seal@users.noreply.github.com>
2023-03-07 12:34:59 +0300

    Update b_compiled.js

--- js/lib/b_compiled.js
+++ js/lib/b_compiled.js
@@ -115,28 +115,28 @@ class RsProjects extends React.Component {
     this.state = {
       reengineering: {
         appname: 'Re-engineering',
-        applink: 'https://github.com/Silas-seal/Innerveil',
+        applink: 'http://20.62.2.131:8080/Innerveilministries',
         appdesc: 'This project was to rebuild a web application using Java technologies. ' + 'The target was to add functionalities, optimize performance and maximize security.',
         mycontr: ['Reverse engineering to acquire new system specifications.', 'Coding and testing a reusable business api for the new app.', 'Writing Java servlets to handle bulk client requests.', 'Implemeting Restful web services to be accessed by both web and app clients.', 'Designed and developed great looking, interactive and responsive front-end pages using React.js & Vue.js', 'Developing and deploying micro-services using SpringBoot', 'Database development and administration [MySQL]', 'Collaborating in system and acceptance testing.', 'Delivery / deployment.'],
         apptech: 'Java, Junit, Jenkins, JavaScript, SQL, Vue.js, React.js, SpringBoot'
       },
       scoasoft: {
         appname: 'Scoasoft',
-        applink: 'https://github.com/Silas-seal/Scoasoft',
+        applink: 'http://20.62.2.131:8080/Reost_Inc/scoasoft.html',
         appdesc: 'Institution management software to automate data' + ' processing and presentation in adminstration departments. These include: Profiles, ' + 'Performance, Payments and Attendance. The target software was both desktop and web.',
         mycontr: ['Analysing and designing the system.', 'Building app GUI Nodes and Panels.', 'Coding and testing of api components.', 'Integration and System testing.', 'Database development and administration.', 'Writing reusable frontend React Componets.', 'Writing modular backend core-services and features using Node.js.'],
         apptech: 'Java, JavaFX, Junit, SQL, React.js, Node.js, Html & CSS'
       },
       jeumxtool: {
         appname: 'JeumxTool',
-        applink: 'https://jeumxtool.herokuapp.com/',
+        applink: 'http://20.62.2.131:8080/JeumxWeb',
         appdesc: 'A Java developer tool targeting JavaFX runtime and application packaging ' + 'on desktop platforms. It\'s a self inspired project, so I developed it  from scratch.',
         mycontr: ['Structured and designed specifications.', 'Developed the components ' + 'and assembled a single tool.', 'Tested and debugged the software.', 'Developed a website for the developed tool.', 'Deployed a functional tool.'],
         apptech: 'Java, JavaFX, Junit, Batch, Vue.js, Python (Django), Sqlite3, Html & CSS'
       },
       rswebapp: {
-        appname: 'Remote Station webapp',
-        applink: 'https://remotestation.herokuapp.com/',
+        appname: 'Reost, Inc webapp',
+        applink: 'https://reost-co.github.io',
         appdesc: 'On-going revision of our web application at Remote Station. ' + 'The Objective is to simply the app, make it lightweight and more responsive using ' + 'React on frontend and Python (flask) on the backend.',
         mycontr: ['Writing reusable React Components.', 'Coding and testing features in Python.', 'Implementing web services with flask.', 'Database development and administration.', 'Development an AI for client engagement, writing clean, readable, reusable and well documented Python Code in Jupyter Notebooks.', 'Security monitoring.'],
         apptech: 'Python, Flask, AI (Machine Learning), React.js, SQL, Html & CSS'
